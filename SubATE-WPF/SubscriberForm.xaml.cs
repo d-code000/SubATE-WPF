0cb40267d68b729888fd13ff4ffbd5c62204d1eb
Disha <disha.wind@gmail.com>
2025-01-10 13:10:25 +0300

    Add SubscriberForm.xaml ui

---
+++ SubATE-WPF/SubscriberForm.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace SubATE_WPF;
+
+public partial class SubscriberForm : Window
+{
+    public SubscriberForm()
+    {
+        InitializeComponent();
+    }
+
+    private void SaveButton_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+}
